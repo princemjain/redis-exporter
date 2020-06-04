016681151802d852b0461c5a4bcfa6e9258fd9bb
Prince M Jain <prince.jain.aux@go-jek.com>
2020-06-04 13:00:00 +0800

    Prince | adding help text in flags

--- main.go
+++ main.go
@@ -10,19 +10,19 @@ import (
 
 var (
 	// Redis configuration arguments
-	hostName   = kingpin.Flag("hostname", "").Default("127.0.0.1").Short('h').String()
-	port       = kingpin.Flag("port", "").Short('p').Default("6379").Int()
-	password   = kingpin.Flag("password", "").Default("").String()
-	batchLimit = kingpin.Flag("batch-limit", "").Default("50").Int64()
+	hostName   = kingpin.Flag("hostname", "Redis master server hostname(any one)").Default("127.0.0.1").Short('h').String()
+	port       = kingpin.Flag("port", "Redis server port").Short('p').Default("6379").Int()
+	password   = kingpin.Flag("password", "Redis server password").String()
+	batchLimit = kingpin.Flag("batch-limit", "Number of records limit to fetch").Default("50").Int64()
 
 	// Input configuration arguments
-	keyPattern = kingpin.Flag("key-pattern", "").String()
+	keyPattern = kingpin.Flag("key-pattern", "Regex pattern to find keys").Default("*").String()
 
 	// Output configuration arguments
-	outputFilePath = kingpin.Flag("output-file-path", "").Short('o').Default("data.csv").String()
-	mergeKey = kingpin.Flag("merge-key", "").Bool()
-	keySplitPattern = kingpin.Flag("key-split-pattern", "").Default("").String()
-	valueSplitPattern = kingpin.Flag("value-split-pattern", "").Default("").String()
+	outputFilePath = kingpin.Flag("output-file-path", "Destination file path").Short('o').Default("data.csv").String()
+	mergeKey = kingpin.Flag("merge-key", "Add record key to output file").Bool()
+	keySplitPattern = kingpin.Flag("key-split-pattern", "Before split the key format").String()
+	valueSplitPattern = kingpin.Flag("value-split-pattern", "Before split the value format(ex: delimiter `:` )").String()
 
 	// redis-exporter configuration arguments
 	version = kingpin.Flag("version", "").Short('v') .Bool()
